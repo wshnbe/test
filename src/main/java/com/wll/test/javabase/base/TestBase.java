19b9d7594dd6e8a17aebe2759b4349dd46c75eea
wll <wshnbe@sina.com>
2018-04-02 09:48:34 +0800

    20180402

--- src/main/java/com/wll/test/javabase/base/TestBase.java
+++ src/main/java/com/wll/test/javabase/base/TestBase.java
@@ -7,8 +7,21 @@ import org.junit.Test;
 public class TestBase {
 
 	@Test
-	public void testBigInteger(){
+	public void testBigInteger() {
 		BigInteger num = new BigInteger("");
 		System.out.println(num);
+	}
+
+	@Test
+	public void showNum() {
+		for (int i = 0; i < 200; i++) {
+			long time = System.currentTimeMillis();
+			long s = time % 2;
+			if (s == 0) {
+				System.out.println(0);
+			} else if (s == 1) {
+				System.out.println(1);
+			}
+		}
 	}
 }
